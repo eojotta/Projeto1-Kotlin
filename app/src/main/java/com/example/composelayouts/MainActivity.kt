891edfc0f5c6a9736f5309e5c8cdbbc51574fbf0
JOÃO PEDRO DOS SANTOS ROMUALDO <25203692@dsn127m1241955.cfp127aluno.local>
2026-09-15 16:56:50 -0300

    primeira aula do projeto

---
+++ app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.composelayouts
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeLayoutsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()){ innerPadding ->
+                    LayoutScreen(modifier = Modifier.padding(innerPadding))
+
+                    }
+                }
+            }
+        }
+    }
+@Composable
+fun LayoutScreen(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier
+            .background(Color.Gray)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    )
+    {
+        Column(
+            modifier = Modifier.background(Color.LightGray)
+                .weight(1f)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+
+        ) {
+            Text("Texto 1")
+            Text("Texto 2")
+            Text("Texto 3")
+        }
+
+        Row(
+            modifier = Modifier.background(Color.Red)
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+            Text("Texto 4")
+            Text("Texto 5")
+            Text("Texto 6")
+        }
+
+    }
+}
